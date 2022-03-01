f4eae8cd2e2f869ed0c3467aa3bef0c915200b59
“thatoneIvan” <ivanh.sanchez84@gmail.com>
2022-02-28 21:58:08 -0600

    JSBasicsExcercise

---
+++ script.js
@@ -0,0 +1,48 @@
+/* 1 */
+console.log(1984);
+
+/* 2 */
+/* Numbers, Strings, Booleans, Null, And Undefined*/
+
+/* 3 */
+/*
++ addition,
+- subtraction, 
+* multiplication, 
+/ division, 
+** Exponentiation
+% Modulus
+*/
+
+/* 4 */
+console.log(6 % 5) //1 
+console.log(10%5 + 1);//1
+
+
+/* 5 */
+console.log('I\M MARY POPPINS, Y\ALL');//I'M MARY POPPINS,Y'ALL
+
+
+/* 6 */
+console.log("Hawk" + "Eye");//Hawkeye
+
+/* 7 */
+console.log("Ivan".length + "Sanchez".length);//11 
+
+/* 8 */
+console.log("Avengers: Endgame"[16]);//e
+
+/* BONUS */
+/* 1 */
+console.log('\\');
+
+/* 2 */
+console.log(5**4-9);//616
+
+/* 3 */
+
+/* 4 */
+
+/* 5 */
+
+/* 6 */
